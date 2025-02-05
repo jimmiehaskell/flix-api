829e0e34c92e54d1bb8b57a5ec95754a9adb0a3a
Jimmie Haskell <haskell4228@gmail.com>
2025-02-05 18:11:59 -0300

    updated urls.py file

--- app/urls.py
+++ app/urls.py
@@ -1,13 +1,6 @@
 from django.contrib import admin
 from django.http import JsonResponse
 from django.urls import path
-
-def hello_view(request):
-    return JsonResponse({
-        'hello': 'Hello World!'
-    })
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', hello_view),
 ]
