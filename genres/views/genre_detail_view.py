4e9e0cf1e200c03f270f455cc4f080937198f263
Jimmie Haskell <haskell4228@gmail.com>
2025-02-05 22:54:54 -0300

    finish crud

--- genres/views/genre_detail_view.py
+++ genres/views/genre_detail_view.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
@@ -8,8 +10,31 @@ from genres.models import Genre
 @csrf_exempt
 def genre_detail_view(request, pk) -> JsonResponse:
     genre = get_object_or_404(Genre, pk=pk)
-    data = {
-        'id': genre.id,
-        'name': genre.name,
-    }
-    return JsonResponse(data, status=200)
+
+    if request.method == 'GET':
+        data = {
+            'id': genre.id,
+            'name': genre.name,
+        }
+        return JsonResponse(data, status=200)
+
+    elif request.method == 'PUT':
+        data = json.loads(request.body.decode('utf-8'))
+        genre.name = data['name']
+        genre.save()
+        return JsonResponse(
+            data={
+                'id': genre.id,
+                'name': genre.name
+            },
+            status=201
+        )
+
+    elif request.method == 'DELETE':
+        genre.delete()
+        return JsonResponse(
+            data={
+                'message': 'Genre deleted'
+            },
+            status=204
+        )
