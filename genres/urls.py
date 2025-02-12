782f8cc401738e19b4c00e28db4f9c4026ef3264
Jimmie Haskell <haskell4228@gmail.com>
2025-02-11 22:26:39 -0300

    finished CRUD using DRF

--- genres/urls.py
+++ genres/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
 from genres.views import GenreListView, GenreCreateView, GenreDetailView, \
-    GenreDeleteView
+    GenreDeleteView, GenreUpdateView
 
 urlpatterns = [
     path('genres/', GenreListView.as_view(), name='genre-list'),
     path('genres/create/', GenreCreateView.as_view(), name='genre-create'),
     path('genres/<int:pk>/', GenreDetailView.as_view(), name='genre-detail'),
-    path('genres/<int:pk>/delete', GenreDeleteView.as_view(), name='genre-delete'),
+    path('genres/<int:pk>/delete', GenreDeleteView.as_view(),
+         name='genre-delete'),
+    path('genres/<int:pk>/update', GenreUpdateView.as_view(),
+         name='genre-update'),
 ]
--- genres/views/__init__.py
+++ genres/views/__init__.py
@@ -1,4 +1,5 @@
 from genres.views.genre_create_view import GenreCreateView
+from genres.views.genre_delete_view import GenreDeleteView
 from genres.views.genre_detail_view import GenreDetailView
 from genres.views.genre_list_view import GenreListView
-from genres.views.genre_delete_view import GenreDeleteView
+from genres.views.genre_update_view import GenreUpdateView
