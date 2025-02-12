536fa9421e6a522917b466fe5236570efdb9d371
Jimmie Haskell <haskell4228@gmail.com>
2025-02-11 22:45:34 -0300

    updated urls.py file

--- genres/urls.py
+++ genres/urls.py
@@ -7,8 +7,6 @@ urlpatterns = [
     path('genres/', GenreListView.as_view(), name='genre-list'),
     path('genres/create/', GenreCreateView.as_view(), name='genre-create'),
     path('genres/<int:pk>/', GenreDetailView.as_view(), name='genre-detail'),
-    path('genres/<int:pk>/delete', GenreDeleteView.as_view(),
-         name='genre-delete'),
-    path('genres/<int:pk>/update', GenreUpdateView.as_view(),
-         name='genre-update'),
+    path('genres/<int:pk>/delete', GenreDeleteView.as_view(), name='genre-delete'),
+    path('genres/<int:pk>/update', GenreUpdateView.as_view(), name='genre-update'),
 ]
