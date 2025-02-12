74e6e573e7e4f8eef12e5e4df6281e9db0962f61
Jimmie Haskell <haskell4228@gmail.com>
2025-02-11 22:15:54 -0300

    created 'delete view'

--- genres/urls.py
+++ genres/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from genres.views import GenreListView, GenreCreateView, GenreDetailView
+from genres.views import GenreListView, GenreCreateView, GenreDetailView, \
+    GenreDeleteView
 
 urlpatterns = [
     path('genres/', GenreListView.as_view(), name='genre-list'),
     path('genres/create/', GenreCreateView.as_view(), name='genre-create'),
     path('genres/<int:pk>/', GenreDetailView.as_view(), name='genre-detail'),
+    path('genres/<int:pk>/delete', GenreDeleteView.as_view(), name='genre-delete'),
 ]
--- genres/views/__init__.py
+++ genres/views/__init__.py
@@ -1,3 +1,4 @@
 from genres.views.genre_create_view import GenreCreateView
 from genres.views.genre_detail_view import GenreDetailView
 from genres.views.genre_list_view import GenreListView
+from genres.views.genre_delete_view import GenreDeleteView
